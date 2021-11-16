f4b7e38fcb4eb88623daa73764e3252586f2a5eb
edwintantawi <edwintantawi@gmail.com>
2021-11-16 17:09:56 +0700

    test

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const multer = require('multer');
+const cookieParser = require('cookie-parser');
+const path = require('path');
 const { Api, TelegramClient } = require('telegram');
 const { CustomFile } = require('telegram/client/uploads');
 const { StringSession } = require('telegram/sessions');
@@ -22,17 +24,24 @@ const API_HASH = process.env.API_HASH;
 // jwt
 const JWT_KEY = process.env.JWT_KEY;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.json());
+app.use(cookieParser());
+
+const memoryDB = [];
+
+const stringSession = new StringSession('');
+const client = new TelegramClient(stringSession, API_ID, API_HASH, {
+  connectionRetries: 5,
+});
+client.connect();
 
 app.post('/auth/sendcode', async (req, res) => {
   const { phoneNumber } = req.body;
 
-  const stringSession = new StringSession('');
-  const client = new TelegramClient(stringSession, API_ID, API_HASH, {
-    connectionRetries: 5,
-  });
-
-  await client.connect();
+  // await client.connect();
   const result = await client.invoke(
     new Api.auth.SendCode({
       phoneNumber,
@@ -52,12 +61,12 @@ app.post('/auth/sendcode', async (req, res) => {
 app.post('/auth/signin', async (req, res) => {
   const { phoneNumber, phoneCodeHash, phoneCode } = req.body;
 
-  const stringSession = new StringSession('');
-  const client = new TelegramClient(stringSession, API_ID, API_HASH, {
-    connectionRetries: 5,
-  });
+  // const stringSession = new StringSession('');
+  // const client = new TelegramClient(stringSession, API_ID, API_HASH, {
+  //   connectionRetries: 5,
+  // });
 
-  await client.connect();
+  // await client.connect();
   await client.invoke(
     new Api.auth.SignIn({
       phoneNumber,
@@ -68,6 +77,7 @@ app.post('/auth/signin', async (req, res) => {
 
   const savedStringSession = client.session.save();
   const credential = jwt.sign({ stringSession: savedStringSession }, JWT_KEY);
+  res.cookie('credential', credential);
   console.log(credential);
   return res.json({ credential });
 });
@@ -136,7 +146,6 @@ app.post('/uploads', upload.any(), authMiddleware, async (req, res) => {
   await client.connect();
 
   const file = req.files[0];
-  console.log(file);
 
   const result = await client.invoke(
     new Api.messages.SendMedia({
@@ -159,7 +168,31 @@ app.post('/uploads', upload.any(), authMiddleware, async (req, res) => {
     })
   );
 
-  return res.json({ id: result.updates[0].id });
+  memoryDB.push({
+    id: result.updates[0].id,
+    title: file.originalname,
+    mimeType: file.mimetype,
+    url: `/uploads/${result.updates[0].id}`,
+  });
+  return res.json({
+    id: result.updates[0].id,
+    title: file.originalname,
+    mimeType: file.mimetype,
+  });
+});
+
+app.get('/uploads', authMiddleware, async (req, res) => {
+  const {
+    credential: { stringSession },
+  } = req.auth;
+
+  const telegramStringSession = new StringSession(stringSession);
+  const client = new TelegramClient(telegramStringSession, API_ID, API_HASH, {
+    connectionRetries: 5,
+  });
+  await client.connect();
+
+  return res.json(memoryDB);
 });
 
 app.get('/uploads/:id', authMiddleware, async (req, res) => {
@@ -191,6 +224,14 @@ app.get('/uploads/:id', authMiddleware, async (req, res) => {
     res.set('Content-Disposition', `attachment; filename="unknown"`);
   }
   return res.send(resultFile);
+});
+
+app.get('/', authMiddleware, (req, res) => {
+  return res.render('index');
+});
+
+app.get('/app/login', (req, res) => {
+  return res.render('login');
 });
 
 /*
--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -3,9 +3,12 @@ const jwt = require('jsonwebtoken');
 const JWT_KEY = process.env.JWT_KEY;
 
 const authMiddleware = (req, res, next) => {
-  const { credential } = req.body;
+  // const { credential } = req.cookies;
+  const credential = req.cookies.credential || req.body.credential;
+  // const { credential } = req.body;
   if (!credential) {
-    return res.json({ error: 'string session not found' });
+    // return res.json({ error: 'string session not found' });
+    res.redirect('/app/login');
   }
 
   const decodedCredential = jwt.verify(credential, JWT_KEY);
